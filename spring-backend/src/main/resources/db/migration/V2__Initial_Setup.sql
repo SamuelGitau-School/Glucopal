c8fb9bbc71c87228191cf119f0cb84bddc4bde6b
Samuelt5gaming <samuelwamukui12@gmail.com>
2026-04-24 16:53:23 +0300

    V2 schema correction

--- spring-backend/src/main/resources/db/migration/V2__Initial_Setup.sql
+++ spring-backend/src/main/resources/db/migration/V2__Initial_Setup.sql
@@ -1,6 +1,8 @@
-DROP VIEW IF EXISTS your_glucose_view_name;
+DROP VIEW IF EXISTS v_active_glucose_records;
 
 ALTER TABLE glucose_records ALTER COLUMN id SET DATA TYPE bigint;
 
-CREATE VIEW your_glucose_view_name AS
-SELECT ... FROM glucose_records;
+CREATE VIEW v_active_glucose_records AS
+    SELECT *
+    FROM glucose_records
+    WHERE is_active = true;
