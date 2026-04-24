d699e5f1823ac53a651f701b1859f34a229ee69c
Samuelt5gaming <samuelwamukui12@gmail.com>
2026-04-24 17:15:22 +0300

    2

--- spring-backend/src/main/resources/db/migration/V2__Initial_Setup.sql
+++ spring-backend/src/main/resources/db/migration/V2__Initial_Setup.sql
@@ -1,7 +1,5 @@
-
-
 DROP VIEW IF EXISTS v_active_glucose_records CASCADE;
-
+DROP VIEW IF EXISTS v_today_glucose_records CASCADE;
 
 ALTER TABLE glucose_records ALTER COLUMN id SET DATA TYPE bigint;
 
