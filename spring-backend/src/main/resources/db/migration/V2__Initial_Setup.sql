b18d0516d11611d9278bf6525c97060c7661d394
Samuelt5gaming <samuelwamukui12@gmail.com>
2026-04-24 17:05:21 +0300

    railway db fix

--- spring-backend/src/main/resources/db/migration/V2__Initial_Setup.sql
+++ spring-backend/src/main/resources/db/migration/V2__Initial_Setup.sql
@@ -1,4 +1,7 @@
-DROP VIEW IF EXISTS v_active_glucose_records;
+
+DROP VIEW IF EXISTS v_today_glucose_records CASCADE;
+DROP VIEW IF EXISTS v_active_glucose_records CASCADE;
+DROP VIEW IF EXISTS your_glucose_view_name CASCADE;
 
 ALTER TABLE glucose_records ALTER COLUMN id SET DATA TYPE bigint;
 
@@ -6,3 +9,8 @@ CREATE VIEW v_active_glucose_records AS
     SELECT *
     FROM glucose_records
     WHERE is_active = true;
+
+CREATE VIEW v_today_glucose_records AS
+    SELECT *
+    FROM v_active_glucose_records
+    WHERE DATE(recorded_at) = CURRENT_DATE;
