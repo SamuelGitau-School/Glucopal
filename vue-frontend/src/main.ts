457f098d05ee42ba568ed4c55a71c49b90daf040
Samuelt5gaming <samuelwamukui12@gmail.com>
2026-05-10 20:51:07 +0300

    corrected the css declaretaion

--- vue-frontend/src/main.ts
+++ vue-frontend/src/main.ts
@@ -1,15 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { useAuthStore } from '@/stores/auth'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
 const app = createApp(App)
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+const pinia = createPinia()
 
+app.use(pinia)
+app.use(router)
+
+const { useAuthStore } = await import('@/stores/auth')
 const auth = useAuthStore()
 await auth.init()
 
